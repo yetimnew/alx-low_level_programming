8433cd949b119c554cc38ab74d7946c27ea5e7f4
yetimnew <yetimnew@gmail.com>
2022-06-03 02:25:44 -0700

    brace created in new lin and space addded

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include "stdio.h"
 
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+ 	puts( "\"Programming is like building a multilingual puzzel");
 	return(0);
 }
+
