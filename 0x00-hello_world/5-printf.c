7c029e58483eb5252352def62d1ad3cfc42c7718
yetimnew <yetimnew@gmail.com>
2022-06-06 00:51:45 -0700

    file 5 printf created

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints script
+ *
+ *Description: prints a script exactly as is
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
